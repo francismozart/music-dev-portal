8a3719d6c923666279116b5a34dc9d6d0e360820
Francis Andrade <francis.andrade@trustly.com>
2024-02-12 19:56:34 -0300

    add client

---
+++ src/app/client/index.ts
@@ -0,0 +1,16 @@
+const SERVER_URL = "http://127.0.0.1:4000";
+const DEFAULT_LOCALE = "en";
+
+export async function getProps(lang: string) {
+  const locale = lang ?? DEFAULT_LOCALE;
+  const globalRes = await fetch(`${SERVER_URL}/global?locale=${locale}`);
+  const global = await globalRes.json();
+
+  const pageRes = await fetch(`${SERVER_URL}/pages?locale=${locale}`);
+  const page = await pageRes.json();
+
+  return {
+    page,
+    global,
+  };
+}
